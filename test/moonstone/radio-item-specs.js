65235d0125b18920eea1e2b825750627dd4d3792
Roy Sutton <roy.sutton@lge.com>
2014-10-01 17:19:30 -0400

    Clean up formatting

--- test/moonstone/radio-item-specs.js
+++ test/moonstone/radio-item-specs.js
@@ -10,83 +10,76 @@ chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 
 // checking sauce credential
 if(!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY){
-    console.warn(
-        '\nPlease configure your sauce credential:\n\n' +
-        'export SAUCE_USERNAME=<SAUCE_USERNAME>\n' +
-        'export SAUCE_ACCESS_KEY=<SAUCE_ACCESS_KEY>\n\n'
-    );
-    throw new Error("Missing sauce credentials");
+	console.warn(
+		'\nPlease configure your sauce credential:\n\n' +
+		'export SAUCE_USERNAME=<SAUCE_USERNAME>\n' +
+		'export SAUCE_ACCESS_KEY=<SAUCE_ACCESS_KEY>\n\n'
+	);
+	throw new Error("Missing sauce credentials");
 }
 
 // http configuration, not needed for simple runs
 wd.configureHttp( {
-    timeout: 60000,
-    retryDelay: 15000,
-    retries: 5
+	timeout: 60000,
+	retryDelay: 15000,
+	retries: 5
 });
 
 var desired = JSON.parse(process.env.DESIRED || '{browserName: "chrome"}');
 desired.name = 'example with ' + desired.browserName;
 desired.tags = ['tutorial'];
 
-wd.addElementPromiseChainMethod(
-  'getClasses',
-  function() {
-    return this
+wd.addElementPromiseChainMethod('getClasses', function() {
+	return this
 		.getAttribute('class').then(function(res) {
 			return res.split(' ');
 		});
-  }
-);
+});
 
-wd.addPromiseChainMethod(
-  'enyoProperty',
-  function(id, prop) {
-    return this.execute("return enyo.$['" + id + "'].get('" + prop + "');");
-  }
-);
-
+wd.addPromiseChainMethod('enyoProperty', function(id, prop) {
+	return this.execute("return enyo.$['" + id + "'].get('" + prop + "');");
+});
 
 describe('Radio Item Sample Test', function() {
-    var browser;
-    var allPassed = true;
+	var browser;
+	var allPassed = true;
 
-    before(function(done) {
-        var username = process.env.SAUCE_USERNAME;
-        var accessKey = process.env.SAUCE_ACCESS_KEY;
-        browser = wd.promiseChainRemote("ondemand.saucelabs.com", 80, username, accessKey);
-        if(process.env.VERBOSE){
-            /* optional logging     
-            browser.on('status', function(info) {
-                console.log(info.cyan);
-            });
-            browser.on('command', function(meth, path, data) {
-                console.log(' > ' + meth.yellow, path.grey, data || '');
-            });*/
-        }
-        browser
+	before(function(done) {
+		var username = process.env.SAUCE_USERNAME;
+		var accessKey = process.env.SAUCE_ACCESS_KEY;
+		browser = wd.promiseChainRemote("ondemand.saucelabs.com", 80, username, accessKey);
+		if(process.env.VERBOSE){
+			/* optional logging	 
+			browser.on('status', function(info) {
+				console.log(info.cyan);
+			});
+			browser.on('command', function(meth, path, data) {
+				console.log(' > ' + meth.yellow, path.grey, data || '');
+			});*/
+		}
+		browser
 			.init(desired)
-            .nodeify(done);
-    });
+			.nodeify(done);
+	});
 
-    afterEach(function(done) {
-        allPassed = allPassed && (this.currentTest.state === 'passed');
-        done();
-    });
+	afterEach(function(done) {
+		allPassed = allPassed && (this.currentTest.state === 'passed');
+		done();
+	});
 
-    after(function(done) {
-        browser
-            .quit()
-            .nodeify(done);
-    });
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
 
-    it("Should have correct title", function(done) {
-        browser
+	it("Should have correct title", function(done) {
+		browser
 			.get(url)
-            .title()
-            .should.become(title)
-            .nodeify(done);
-    });
+			.title()
+			.should.become(title)
+			.nodeify(done);
+	});
 
 	it("Should have correct defaults", function (done) {
 		browser
