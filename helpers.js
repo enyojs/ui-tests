95ac406d13b78b56b0652486626c45f5c92095bf
Roy Sutton <roy.sutton@lge.com>
2014-10-23 19:14:40 -0400

    Updated scripts to centralize initialization and add local testing via Selenium
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

---
+++ helpers.js
@@ -0,0 +1,59 @@
+'use strict';
+module.exports = {
+	initEnvironment: function(wd, title, tags) {
+		var desired = JSON.parse(process.env.DESIRED || '{browserName: "chrome"}');
+
+		if(process.env.SAUCE === 'true') {
+			// checking sauce credential
+			if(!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY){
+				console.warn(
+					'\nPlease configure your sauce credential:\n\n' +
+					'export SAUCE_USERNAME=<SAUCE_USERNAME>\n' +
+					'export SAUCE_ACCESS_KEY=<SAUCE_ACCESS_KEY>\n\n'
+				);
+				throw new Error("Missing sauce credentials");
+			}
+
+			// http configuration, not needed for simple runs
+			wd.configureHttp( {
+				timeout: 90000,
+				retryDelay: 15000,
+				retries: 5
+			});
+			if(process.env.TRAVIS_JOB_NUMBER) {	// Running on travis
+				desired['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
+			}
+		}
+
+		desired.name = title + ' with ' + desired.browserName;
+		desired.tags = tags;
+
+		wd.addElementPromiseChainMethod('getClasses', function() {
+			return this
+				.getAttribute('class').then(function(res) {
+					return res.split(' ');
+				});
+		});
+
+		wd.addPromiseChainMethod('enyoProperty', function(id, prop) {
+			return this.execute("return enyo.$['" + id + "'].get('" + prop + "');");
+		});
+
+		return desired;
+	},
+	initBrowser: function (wd, desired, done) {
+		var browser;
+		if(process.env.SAUCE === 'true') {
+			console.log("Saucy");
+			var username = process.env.SAUCE_USERNAME;
+			var accessKey = process.env.SAUCE_ACCESS_KEY;
+			browser = wd.promiseChainRemote("ondemand.saucelabs.com", 80, username, accessKey);
+		} else {
+			console.log("Sauceless");
+			browser = wd.promiseChainRemote();
+		}
+		browser.init(desired).nodeify(done);
+		return browser;
+	}
+};
+
---
+++ local_desireds.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+	phantomjs: {browserName: 'phantomjs'},
+	chrome: {browserName: 'chrome'},
+	firefox: {browserName: 'firefox'},
+	explorer: {browserName: 'internet explorer'}
+};
