481173f83fb00de14c5d0790e196ca460ddcf888
Roy Sutton <roy.sutton@lge.com>
2015-04-10 16:46:52 -0400

    Added Reset TimePicker test GT-11132
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

---
+++ test/moonstone/TimePicker/Reset-specs.js
@@ -0,0 +1,44 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	url = 'ui-tests/test/loader.html?moonstone/TimePicker/Reset',
+	title = 'Time Picker: Reset Time',
+	tags = ['sample'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should clear time when time set to null', function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(url)
+			.waitForElementById(app.pickerID)
+			.enyoPropertyGet(app.pickerID, 'value').should.eventually.equal(null, 'starts empty')
+			.elementById(app.pickerID)
+				.click()
+			.enyoPropertyGet(app.pickerID, 'value').should.eventually.not.equal(null, 'filled on selection')
+			.elementById(app.resetID)
+				.click()
+			.enyoPropertyGet(app.pickerID, 'value').should.eventually.equal(null, 'cleared on reset')
+			.enyoPropertyGet(app.pickerID, 'open').should.eventually.equal(false, 'closed on reset')
+			.nodeify(done);
+	});
+
+});
+
+app = {
+	pickerID: 'app_pickerTime',
+	resetID: 'app_buttonReset'
+};
+
