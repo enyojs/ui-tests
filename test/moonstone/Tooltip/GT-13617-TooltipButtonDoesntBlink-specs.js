fc74a2ab2189152aa32d6934c10bfb7afad56602
Derek Tor <derek.tor@lge.com>
2015-06-19 13:02:56 -0700

    removed delays

--- test/moonstone/Tooltip/GT-13617-TooltipButtonDoesntBlink-specs.js
+++ test/moonstone/Tooltip/GT-13617-TooltipButtonDoesntBlink-specs.js
@@ -19,16 +19,31 @@ describe(title, function() {
 			.nodeify(done);
 	});
 
-	it('Should not loop hours while the minute picker loops', function (done) {
+	it('Should not blink when cursor is moving up and down within button', function (done) {
 		browser
 			.setWindowSize(1920,1280)
 			.get(url)
-
+			.waitForElementById(app.rightButton)
+			.elementById(app.rightTooltip)
+			.moveTo(0,0)
+			.elementById(app.rightButton)
+			.moveTo(0,0)
+			.delay(1000)
+			.enyoPropertyGet(app.rightTooltip, 'style').should.eventually.not.contain("display: none;")
+			.elementById(app.rightButton)
+			.moveTo(0,-5)
+			.elementById(app.rightButton)
+				.getClasses().should.eventually.contain('spotlight')
+			.elementById(app.rightButton)
+			.moveTo(0,10)
+			.elementById(app.rightButton)
+				.getClasses().should.eventually.contain('spotlight')
 			.nodeify(done);
 	});
 
 });
 
 app = {
-
+	rightButton: "app_button2",
+	rightTooltip: "app_toolTip"
 };
