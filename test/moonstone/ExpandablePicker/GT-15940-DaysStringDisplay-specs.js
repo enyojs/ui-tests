72668b377149d72f0de133f04327945c3aee183a
Derek Tor <derek.tor@lge.com>
2015-08-06 13:51:55 -0700

    added tests for GT-15940

--- test/moonstone/ExpandablePicker/GT-15940-DaysStringDisplay-specs.js
+++ test/moonstone/ExpandablePicker/GT-15940-DaysStringDisplay-specs.js
@@ -23,13 +23,49 @@ describe(title, function() {
 		browser
 			.setWindowSize(1920,1280)
 			.get(url)
-
+			.waitForElementById(app.dayPicker)
+			.click()
+			.delay(500)
+			.waitForElementById(app.sundayId)
+			.click()
+			.delay(500)
+			.elementById(app.mondayId)
+			.click()
+			.delay(500)
+			.elementById(app.tuesdayId)
+			.click()
+			.delay(500)
+			.elementById(app.wednesdayId)
+			.click()
+			.delay(500)
+			.elementById(app.thursdayId)
+			.click()
+			.delay(500)
+			.elementById(app.fridayId)
+			.click()
+			.delay(500)
+			.elementById(app.saturdayId)
+			.click()
+			.delay(500)
+			.elementById(app.dayPicker)
+			.click()
+			.delay(500)
+			.elementById(app.currentValueString)
+			.text().should.eventually.equal("Every Day")
 			.nodeify(done);
 	});
 
 });
 
 app = {
-
+	dayPicker: "app_dayPicker_headerWrapper",
+	sundayId: "app_dayPicker_checkboxItem",
+	mondayId: "app_dayPicker_checkboxItem2",
+	tuesdayId: "app_dayPicker_checkboxItem3",
+	wednesdayId: "app_dayPicker_checkboxItem4",
+	thursdayId: "app_dayPicker_checkboxItem5",
+	fridayId: "app_dayPicker_checkboxItem6",
+	saturdayId: "app_dayPicker_checkboxItem7",
+	currentValueString: "app_dayPicker_currentValue"
 };
 
--- test/moonstone/ExpandablePicker/GT-15940-DaysStringDisplay.js
+++ test/moonstone/ExpandablePicker/GT-15940-DaysStringDisplay.js
@@ -5,105 +5,11 @@ enyo.kind({
 		{kind: "moon.Panels", pattern: "activity", classes: "enyo-fit", components: [
 			{kind: "moon.Panel", name: "nonGroupedPanel", onChange: "pickerChanged", title: "Expandable", headerType: "medium", titleBelow: "Not grouped", style: "width:50%;", components: [
 				{kind: "moon.Scroller", horizontal: "hidden", classes: "enyo-fill", components: [
-					{style: "max-width: 500px;", components: [
-						{kind: "moon.ExpandablePicker", noneText: "Nothing selected", content: "Expandable Picker", allowHtml:true, components: [
-							{content: "English"},
-							{content: "Spanish"},
-							{content: "French"},
-							{content: "German"},
-							{content: "Italian"},
-							{content: "Japanese"},
-							{content: "Symbols <span style='color:orange;'>&#x2620; &#x2764; &#x2619;</span>", allowHtml:true}
-						]},
-						{kind: "moon.ExpandablePicker", content: "Pre-selected Picker", components: [
-							{content: "On", active: true},
-							{content: "Off"}
-						]},
-						{kind: "moon.ExpandablePicker", multipleSelection: true, noneText: "Nothing selected", content: "Non-auto-collapsing", autoCollapseOnSelect: false, components: [
-							{content: "Item 1"},
-							{content: "Item 2", active: true},
-							{content: "Item 3", active: true}
-						]},
-						{kind: "moon.ExpandablePicker", noneText: "Nothing selected with loooooooooooooooooooooooooong text truncation", content: "Expandable Picker with looooooooooooooooooooooooooong text truncation", components: [
-							{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 1"},
-							{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 2"},
-							{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 3"}
-						]},
-						{kind: "moon.ExpandablePicker", disabled:true, content: "Disabled Picker", components: [
-							{content: "Item 1"},
-							{content: "Item 2", active: true},
-							{content: "Item 3"}
-						]},
-						{kind: "moon.ExpandablePicker", content: "Pre-expanded picker", open: true, components: [
-							{content: "Item 1"},
-							{content: "Item 2", active: true},
-							{content: "Item 3"}
-						]},
-						{kind: "moon.ExpandableIntegerPicker", noneText: "Not Selected", autoCollapse: true, content: "Integer Picker", value: 7, min: 3, max: 15, step: 1, unit: "elephants"},
-						{kind: "moon.ExpandableIntegerPicker", noneText: "Not Selected", disabled:true, autoCollapse: true, content: "Disabled Integer Picker", value: 2, min: 1, max: 15, unit: "sec"},
-						{kind: "moon.DatePicker", noneText: "Pick a Date", content: "Date Picker"},
-						{kind: "moon.TimePicker", noneText: "Pick a Date", content: "Time Picker"},
-						{kind: "moon.DayPicker", noneText: "Pick a Day", content: "Day Picker"},
-						{kind: "moon.ExpandableInput", noneText: "Enter text", content: "Expandable Input", placeholder: "Enter text"},
-						{kind: "moon.ExpandableDataPicker", content: "Expandable Data Picker", noneText: "Nothing Selected", components: [
-							{bindings: [
-								{from: ".model.label", to: ".content"}
-							]}
-						]}
+					{style: "max-width: 500px;", components: [						
+						{kind: "moon.DayPicker", noneText: "Pick a Day", content: "Day Picker"}						
 					]}
 				]}
-			]},
-			{kind: "moon.Panel", name: "groupedPanel", onChange: "pickerChanged", title: "Pickers", headerType: "medium", titleBelow: "Grouped", joinToPrev:true, components: [
-				{kind: "enyo.Group", tag:null, highlander: true, components: [
-					{kind: "moon.Scroller", horizontal: "hidden", classes: "enyo-fill", components: [
-						{style: "max-width: 500px;", components: [
-							{kind: "moon.ExpandablePicker", noneText: "Nothing selected", content: "Expandable Picker", allowHtml:true, components: [
-								{content: "English"},
-								{content: "Spanish"},
-								{content: "French"},
-								{content: "German"},
-								{content: "Italian"},
-								{content: "Japanese"},
-								{content: "Symbols <span style='color:orange;'>&#x2620; &#x2764; &#x2619;</span>", allowHtml:true}
-							]},
-							{kind: "moon.ExpandablePicker", content: "Pre-selected Picker", components: [
-								{content: "On", active: true},
-								{content: "Off"}
-							]},
-							{kind: "moon.ExpandablePicker", content: "Non-auto-collapsing", autoCollapseOnSelect: false, components: [
-								{content: "Item 1"},
-								{content: "Item 2", active: true},
-								{content: "Item 3"}
-							]},
-							{kind: "moon.ExpandablePicker", noneText: "Nothing selected with loooooooooooooooooooooooooong text truncation", content: "Expandable Picker with looooooooooooooooooooooooooong text truncation", components: [
-								{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 1"},
-								{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 2"},
-								{content: "Looooooooooooooooooooooooooooooooooooooooooooong Item 3"}
-							]},
-							{kind: "moon.ExpandablePicker", disabled:true, content: "Disabled Picker", components: [
-								{content: "Item 1"},
-								{content: "Item 2", active: true},
-								{content: "Item 3"}
-							]},
-							{kind: "moon.ExpandablePicker", content: "Pre-expanded picker", open: true, components: [
-								{content: "Item 1"},
-								{content: "Item 2", active: true},
-								{content: "Item 3"}
-							]},
-							{kind: "moon.ExpandableIntegerPicker", noneText: "Not Selected", autoCollapse: true, content: "Integer Picker", value: 7, min: 3, max: 15, step: 1, unit: "elephants"},
-							{kind: "moon.ExpandableIntegerPicker", noneText: "Not Selected", disabled:true, autoCollapse: true, content: "Disabled Integer Picker", value: 2, min: 1, max: 15, unit: "sec"},
-							{kind: "moon.DatePicker", noneText: "Pick a Date", content: "Date Picker"},
-							{kind: "moon.TimePicker", noneText: "Pick a Date", content: "Time Picker"},
-							{kind: "moon.ExpandableInput", noneText: "Enter text", content: "Expandable Input", placeholder: "Enter text"},
-							{kind: "moon.ExpandableDataPicker", content: "Expandable Data Picker", noneText: "Nothing Selected", components: [
-								{bindings: [
-									{from: ".model.label", to: ".content"}
-								]}
-							]}
-						]}
-					]}
-				]}
-			]}
+			]},			
 		]}
 	],
 	create: enyo.inherit(function (sup) {
@@ -117,9 +23,6 @@ enyo.kind({
 				{label: "Item 4"},
 				{label: "Item 5"}
 			]);
-
-			this.$.expandableDataPicker.set("collection", c);
-			this.$.expandableDataPicker2.set("collection", c);
 		};
 	}),
 	pickerChanged: function(inSender, inEvent) {
