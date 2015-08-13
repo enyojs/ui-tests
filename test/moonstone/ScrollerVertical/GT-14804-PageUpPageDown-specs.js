174281d83b84f3dc4960b3c420b2f487adfc41a6
Derek Tor <derek.tor@lge.com>
2015-08-13 13:06:26 -0700

    added GT-14804 tests

---
+++ test/moonstone/ScrollerVertical/GT-14804-PageUpPageDown-specs.js
@@ -0,0 +1,52 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	path = 'test/moonstone/ScrollerVertical/GT-14804-PageUpPageDown',
+	title = 'PageUp/Down: USB Keyboard\'s Page Up/Down scrolls content',
+	directory = 'ui-tests/dist',
+	tags = ['moonstone','qa','pageup','pagedn','ScrollerVertical'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, path, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should page up and down when pressed' , function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(directory)
+			.waitForElementById(app.appId)
+			.moveTo()
+			.elementById(app.firstHeader)
+			.moveTo()
+			.elementById(app.vthumb)
+			.getLocation().should.eventually.have.property('y', 330)
+			.keys(helpers.keys.PageDown)
+			.keys(helpers.keys.PageDown)
+			.delay(1000)
+			.elementById(app.vthumb)
+			.getLocationInView().should.eventually.have.property('y').should.be.eventually.above(330)
+			.keys(helpers.keys.PageUp)
+			.keys(helpers.keys.PageUp)
+			.delay(1000)
+			.elementById(app.vthumb)
+			.getLocation().should.eventually.have.property('y', 330)
+			.nodeify(done);
+	});
+
+});
+
+app = {
+	appId: 'gT-14804-PageUpPageDown',
+	vthumb: 'gT-14804-PageUpPageDown_scroller_strategy_vthumb',
+	firstHeader: 'gT-14804-PageUpPageDown_expandablePicker_header_header'
+};
