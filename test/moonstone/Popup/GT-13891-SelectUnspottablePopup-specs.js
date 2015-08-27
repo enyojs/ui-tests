57a8a539abeb7e985e5b73ade453fdbdeeaebbbe
Derek Tor <derek.tor@lge.com>
2015-08-27 16:17:30 -0700

    converted tests for GT-13891 to enyo 2.6

--- test/moonstone/Popup/GT-13891-SelectUnspottablePopup-specs.js
+++ test/moonstone/Popup/GT-13891-SelectUnspottablePopup-specs.js
@@ -1,16 +1,17 @@
-var helpers = rootRequire("./helpers"),
+var helpers = rootRequire('./helpers'),
 	app = {};	// Test-specific settings at bottom of the file
 
 var base = 'http://localhost:3000/',
-	url = 'lib/moonstone/samples/Sample.html?PopupSample',
+	path = 'test/moonstone/Popup/GT-13891-SelectUnspottablePopup',
 	title = 'GT-13891 5-way Select Unspottable Popup',	// Title shows up in test output
+	directory = 'ui-tests/dist',
 	tags = ['moonstone', 'spotlight','QA'];	// Tags show up in SauceLabs test output
 
 describe(title, function() {
 	var browser;
 
 	before(function(done) {
-		browser = helpers.initBrowser(title, tags, base, done);
+		browser = helpers.initBrowser(title, tags, base, path, done);
 	});
 
 	after(function(done) {
@@ -19,10 +20,10 @@ describe(title, function() {
 			.nodeify(done);
 	});
 
-	it("Should not dismiss Popup on Spotlight select", function (done) {
+	it('Should not dismiss Popup on Spotlight select', function (done) {
 		browser
 			.setWindowSize(1920,1280)
-			.get(url)
+			.get(directory)
 			// Wait for the app to load and render our element
 			.waitForElementById(app.BasicPopupButtonId)
 			// Select the first popup option using spotlight controls
@@ -30,17 +31,17 @@ describe(title, function() {
 			.keys(helpers.keys.SpotlightSelect)
 			// Verify the Popup is showing
 			.elementById(app.PopupId)
-				.getClasses().should.eventually.contain('showing', 'popup showing')
+			.getClasses().should.eventually.contain('showing', 'popup showing')
 			// Send a select and verify that it does not cause the popup to close
 			.keys(helpers.keys.SpotlightSelect)
 			.elementById(app.PopupId)
-				.getClasses().should.eventually.contain('showing', 'popup still showing')
+			.getClasses().should.eventually.contain('showing', 'popup still showing')
 			.nodeify(done);
 	});
 
 });
 
 app = {
-	BasicPopupButtonId: 'app_button',
-	PopupId: 'app_basicPopup'
+	BasicPopupButtonId: 'gT-13891-SelectUnspottablePopup_button_client',
+	PopupId: 'gT-13891-SelectUnspottablePopup_basicPopup'
 };
