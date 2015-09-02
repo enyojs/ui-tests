7a6b3590bc94e9cbf53fc8a1f3af0ccf1e0142c1
Roy Sutton <roy.sutton@lge.com>
2015-09-01 17:18:48 -0700

    Fix packaging step in GT-11270
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

--- test/moonstone/TimePicker/GT-11270-DaylightSavings-specs.js
+++ test/moonstone/TimePicker/GT-11270-DaylightSavings-specs.js
@@ -11,7 +11,7 @@ describe(title, function() {
 
 	before(function(done) {
 		helpers.epack(path);
-		browser = helpers.initBrowser(title, tags, base, done);
+		browser = helpers.initBrowser(title, tags, base, path, done);
 	});
 
 	after(function(done) {
