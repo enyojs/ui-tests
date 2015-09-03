bfc6ede13c4530bae7e369d5635ef5bc8c74fd6d
Derek Tor <derek.tor@lge.com>
2015-09-03 15:56:56 -0700

    added tests for GT-11261

---
+++ test/moonstone/SimplePicker/GT-11261-StartsWhenButtonSpotlight-specs.js
@@ -0,0 +1,69 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	path = 'test/moonstone/SimplePicker/GT-11261-StartsWhenButtonSpotlight',
+	title = 'SimplePicker: Marquee Starts when Button Obtains Spotlight',
+	directory = 'ui-tests/dist',
+	tags = ['Marquee', 'SimplePicker', 'QA'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, path, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should start animation on button spotlight', function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(directory)
+			.waitForElementById(app.appId)
+			.keys(helpers.keys.SpotlightDown)
+			.delay(400)
+			//check if left button is disabled
+			.elementById(app.buttonLeft)
+			.getClasses().should.eventually.contain('disabled')
+			//make sure spotlight is on right button
+			.elementById(app.buttonRight)
+			.getClasses().should.eventually.contain('spotlight')
+			.keys(helpers.keys.SpotlightSelect)
+			.delay(400)
+			.elementById(app.buttonRight)
+			.getClasses().should.eventually.contain('spotlight')
+			.keys(helpers.keys.SpotlightSelect)
+			.delay(400)
+			.elementById(app.buttonRight)
+			.getClasses().should.eventually.contain('spotlight')
+			.keys(helpers.keys.SpotlightSelect)
+			.delay(400)
+			//check if right button is disabled
+			.elementById(app.buttonRight)
+			.getClasses().should.eventually.contain('disabled')
+			//left button should receive spotlight once right button is disabled
+			.elementById(app.buttonLeft)
+			.getClasses().should.eventually.contain('spotlight')
+			//need to delay for element to appear
+			.elementById(app.buttonRight)
+			.delay(1000)
+			.elementById(app.hebrewText)
+			.getClasses().should.eventually.contain(app.animateMarqueeClass)
+			.nodeify(done);
+	});
+
+});
+
+app = {
+	appId: 'gT-11261-StartsWhenButtonSpotlight',
+	buttonRight: 'gT-11261-StartsWhenButtonSpotlight_picker1_buttonRight',
+	buttonLeft: 'gT-11261-StartsWhenButtonSpotlight_picker1_buttonLeft',
+	hebrewText: 'gT-11261-StartsWhenButtonSpotlight_marqueeText4_marqueeText',
+	transition: '4.86666666666667s linear',
+	animateMarqueeClass: 'animate-marquee'
+};
---
+++ test/moonstone/SimplePicker/GT-11261-StartsWhenButtonSpotlight/GT-11261-StartsWhenButtonSpotlight.js
@@ -0,0 +1,65 @@
+var
+	kind = require('enyo/kind');
+
+var
+	FittableRows = require('layout/FittableRows');
+
+var
+	BodyText = require('moonstone/BodyText'),
+	Divider = require('moonstone/Divider'),
+	Scroller = require('moonstone/Scroller'),
+	SimplePicker = require('moonstone/SimplePicker'),
+	load = require('../../../load'),
+	Test = kind({
+		name: 'test.GT-11261-StartsWhenButtonSpotlight',
+		kind: FittableRows,
+		classes: 'moon moon-sample-padded enyo-unselectable enyo-fit',
+		components:[
+			{kind: Scroller, fit: true, components: [
+				{kind: Divider, content: 'Picker 1 & 2: Animated'},
+				{kind: SimplePicker, name: 'picker1', onChange: 'changed', components: [
+					{content: 'San Francisco Airport Terminal Gate 1', active: true},
+					{content: 'Boston Airport Terminal Gate 2'},
+					{content: 'Tokyo Airport Terminal Gate 3'},
+					{content: 'נמל התעופה בן גוריון טרמינל הבינלאומי'}
+				]},
+			]},
+			{components: [
+				{kind: Divider, content: 'Result'},
+				{kind: BodyText, name: 'result', content: 'No change yet'}
+			]}
+		],
+		toggleShowing: function () {
+			this.$.picker6.setShowing(!this.$.picker6.showing);
+		},
+		changed: function (sender, event) {
+			this.$.result.setContent(sender.name + ' changed to ' + event.content + ' (' + event.index + ')');
+		},
+		changeItem: function () {
+			var picker = this.$['picker' + (this.$.which.getSelectedIndex()+1)];
+			var val = parseInt(this.$.changeInput.getValue(),10);
+			var len = picker.getClientControls().length - 1;
+			if (isNaN(val) || val < 0 || val > len) {
+				this.$.result.setContent(picker.name + ' value must be an integer between 0-' + len);
+			} else {
+				picker.setSelectedIndex(val);
+			}
+		},
+		addItem: function () {
+			if (!this.$.addInput.getValue()) {
+				this.$.result.setContent('Please insert content value.');
+				return;
+			}
+			var picker = this.$['picker' + (this.$.which.getSelectedIndex()+1)];
+			picker.createComponent({content:this.$.addInput.getValue()}).render();
+			this.$.result.setContent('\'' + this.$.addInput.getValue() + '\' is added to ' + picker.name);
+		},
+		destroyItem: function () {
+			var picker = this.$['picker' + (this.$.which.getSelectedIndex()+1)];
+			var sel = picker.getSelected();
+			if (sel) {
+				sel.destroy();
+			}
+		}
+	});
+load(Test);
