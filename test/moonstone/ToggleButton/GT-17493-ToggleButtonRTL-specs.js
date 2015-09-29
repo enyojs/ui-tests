96381201bc06021fd2011c5cc75c96413aecccc6
Derek Tor <derek.tor@lge.com>
2015-09-29 15:37:53 -0700

    added css check for :after

--- test/moonstone/ToggleButton/GT-17493-ToggleButtonRTL-specs.js
+++ test/moonstone/ToggleButton/GT-17493-ToggleButtonRTL-specs.js
@@ -28,11 +28,17 @@ describe(title, function() {
 			.getComputedCss('direction').should.eventually.equal('rtl')
 			.elementByTagName('body')
 			.getClasses().should.eventually.contain('enyo-locale-right-to-left')
+			//must use getComputedStyle because getComputedCss cannot find pseudo elements
+			.execute('return getComputedStyle(document.getElementById("'+app.toggleButton8+'"), ":after").getPropertyValue("right")')
+			.should.eventually.equal('auto')
+			.execute('return getComputedStyle(document.getElementById("'+app.toggleButton8+'"), ":after").getPropertyValue("left")')
+			.should.eventually.not.equal('auto')
 			.nodeify(done);
 	});
 
 });
 
 app = {
-	appId: 'gT-17493-ToggleButtonRTL'
+	appId: 'gT-17493-ToggleButtonRTL',
+	toggleButton8: 'gT-17493-ToggleButtonRTL_toggleButton8'
 };
