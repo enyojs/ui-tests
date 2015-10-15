5cd65fa886744ef7c5155c56941533b2a443ac9d
Derek Tor <derek.tor@lge.com>
2015-10-15 13:47:32 -0700

    Added tests for GT-14263

---
+++ test/moonstone/ListAction/GT-14263-ListActionCollapseOnBack-specs.js
@@ -0,0 +1,51 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	path = 'test/moonstone/ListAction/GT-14263-ListActionCollapseOnBack',
+	title = 'BackKey: ListActions Collapses upon Back Key',
+	directory = 'ui-tests/dist',
+	tags = ['moonstone','qa','ListActions','BackKey'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, path, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should collapse list on when back is pressed' , function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(directory)
+			.waitForElementById(app.appId)
+			.keys(helpers.keys.SpotlightUp)
+			.delay(500)
+			.elementById(app.dummyIcon)
+			.getClasses().should.eventually.contain('spotlight')
+			.keys(helpers.keys.SpotlightSelect)
+			.delay(500)
+			.elementById(app.drawerClient)
+			.getClasses().should.eventually.contain('open')
+			.keys(helpers.keys.SpotlightDown)
+			.delay(500)
+			.back()
+			.delay(500)
+			.elementById(app.dummyIcon)
+			.getClasses().should.eventually.contain('spotlight')
+			.nodeify(done);
+	});
+
+});
+
+app = {
+	appId: 'gT-14263-ListActionCollapseOnBack',
+	dummyIcon: 'gT-14263-ListActionCollapseOnBack_listActions2_activator',
+	drawerClient: 'gT-14263-ListActionCollapseOnBack_listActions2_drawer_client'
+};
