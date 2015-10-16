d539202fbc52cec022725ca5c8db84771bdecd15
Derek Tor <derek.tor@lge.com>
2015-10-16 04:20:31 -0700

    added tests GT-14269

---
+++ test/moonstone/Popup/GT-14269-FocusReturnsContextualPopup-specs.js
@@ -0,0 +1,46 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	path = 'test/moonstone/Popup/GT-14269-FocusReturnsContextualPopup',
+	title = 'BackKey: Focus returns to Parent in ContextualPopup',
+	directory = 'ui-tests/dist',
+	tags = ['moonstone','qa','BackKey','ContextualPopup'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, path, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should return focus to parent after back key' , function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(directory)
+			.waitForElementById(app.appId)
+			.keys([helpers.keys.SpotlightRight, helpers.keys.SpotlightRight, helpers.keys.SpotlightDown, helpers.keys.SpotlightDown])
+			.elementById(app.rightButtonId)
+			.getClasses().should.eventually.contain('spotlight')
+			.keys(helpers.keys.SpotlightSelect)
+			.elementById(app.rightButtonPopup)
+			.getComputedCss('display').should.eventually.not.equal('none')
+			.back()
+			.elementById(app.rightButtonPopup)
+			.getComputedCss('display').should.eventually.equal('none')
+			.nodeify(done);
+	});
+
+});
+
+app = {
+	appId: 'gT-14269-FocusReturnsContextualPopup',
+	rightButtonId: 'gT-14269-FocusReturnsContextualPopup_contextualPopupButton5',
+	rightButtonPopup: 'gT-14269-FocusReturnsContextualPopup_contextualPopup4'
+};
