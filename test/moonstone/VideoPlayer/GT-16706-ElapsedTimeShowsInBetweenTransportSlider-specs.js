6383b6657c7834f853ef66469e33ee0c102f1821
Derek Tor <derek.tor@lge.com>
2015-12-09 11:43:30 -0800

    Added tests for GT-16706

---
+++ test/moonstone/VideoPlayer/GT-16706-ElapsedTimeShowsInBetweenTransportSlider-specs.js
@@ -0,0 +1,71 @@
+var helpers = rootRequire('./helpers'),
+	app = {};	// Test-specific settings at bottom of the file
+
+var base = 'http://localhost:3000/',
+	path = 'test/moonstone/VideoPlayer/GT-16706-ElapsedTimeShowsInBetweenTransportSlider',
+	title = 'GT-16706 - VideoPlayer: Elapsed time\'s indicator partially displays behind the Video TransportSlider',
+	directory = 'ui-tests/dist',
+	tags = ['moonstone','qa','VideoPlayer'];	// Tags show up in SauceLabs test output
+
+describe(title, function() {
+	var browser;
+	var knobPosition = 0;
+
+	before(function(done) {
+		browser = helpers.initBrowser(title, tags, base, path, done);
+	});
+
+	after(function(done) {
+		browser
+			.quit()
+			.nodeify(done);
+	});
+
+	it('should have elapsed time indicator shows partially behind Video Transport Slider' , function (done) {
+		browser
+			.setWindowSize(1920,1280)
+			.get(directory)
+			.waitForElementById(app.videoPlayer)
+			.delay(5000)
+			.enyoPropertyGet(app.videoPlayer, 'onloadeddata').should.eventually.equal('dataloaded')
+			.keys([helpers.keys.SpotlightDown, helpers.keys.SpotlightDown])
+			.delay(1000)
+			.elementById(app.playerControl)
+			.getComputedCss('display').should.eventually.not.equal('none')			
+			.elementById(app.progressBar)
+			.moveTo()
+			.moveTo(100, 50)
+			.moveTo()
+			.click()
+			.delay(500)
+			.elementById(app.knob)
+			.getComputedCss('left').then(setPosition)
+			.elementById(app.elapsedTimeBar)
+			.getComputedCss('width').then(checkPosition)
+			.nodeify(done);
+	});
+
+	var setPosition = function(response){
+		knobPosition = parseInt(response);
+	};
+
+	/*
+	Check that pixels width of slider bar is within the width of the knob
+	knob's width is 96px and scaled 0.5 so width is 48px. 
+	For progress bar to be within then the knob and the progress bar width needs to be within 24px of each side 
+	*/
+	var checkPosition = function(response){
+		var widthPosition = parseInt(response);
+		widthPosition.should.be.closeTo(knobPosition, 24);
+	};
+
+});
+
+app = {
+	videoPlayer: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_video',
+	playerControl: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_playerControl',
+	progressBar: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_slider',
+	elapsedTimeBar: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_slider_bar',
+	knob: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_slider_knob',
+	playButton: 'gT-16706-ElapsedTimeShowsInBetweenTransportSlider_player_fsPlayPause'
+};
